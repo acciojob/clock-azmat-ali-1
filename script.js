0d1b4e465c5137fa989c3258da86e209331f5684
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-04-25 03:28:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function updateTime() {
+  const now = new Date();
+  const dateTimeString = now.toLocaleString();
+  document.getElementById("timer").innerText = dateTimeString;
+}
+
+setInterval(updateTime, 1000);
